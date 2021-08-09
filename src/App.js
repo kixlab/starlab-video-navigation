01d25758b1488ae014a96e8a97fe6fc67270f62b
sangkyung <skkwak9806@kaist.ac.kr>
2021-08-10 00:28:18 +0900

    baseline complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NavigationPage from './NavigationPage';
+import NavigationPageWithGraph from './NavigationPageWithGraph';
 import EditPage from './EditPage';
+import NavigationPage from './NavigationPage';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Switch>
         <Route path='/edit' component={EditPage} />
+        <Route path='/graph' component={NavigationPageWithGraph} />
         <Route path='/' component={NavigationPage} />
       </Switch>
     </Router>
--- src/NavigationPage.js
+++ src/NavigationPage.js
@@ -1,159 +1,206 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import ReactTooltip from 'react-tooltip';
 import './NavigationPage.css';
-import Graph from "react-graph-vis";
 
 import video from "./video/The Best Steak Wrap - Clean Eating.mp4";
-
-import actions from './data/atomic_action.json';
 import sceneGraphs from './data/how_to_100m_data_sg.json';
 
 
 function NavigationPage() {
+  const [objectList, setObjectList] = useState();
+  const [selectedObj, setObject] = useState([]);
+  const [objTime, setObjTime] = useState();
 
-  const [atomicActions, setActions] = useState(actions);
-  const [actionGraphs, setActionGraphs] = useState();
-  const [aggGraph, setAggGraph] = useState();
+  const [currentTime, setCurrentTime] = useState(0);
+  // const [metadata, setMetadata] = useState(null);
+  const [playbarPosition, setplaybarPosition] = useState(0);
+
+  const ref = useRef();
+  const totalTime = (ref && ref.current && ref.current.duration) || 0;
+  const playbarElement = document.getElementsByClassName('playbar');
+
 
   useEffect (() => {
-    parse_action_graph();
-    parse_agg_graph();
+    parse_objects();
+    addTimeUpdate();
+    const setFromEvent = (e) => setplaybarPosition(e.clientX - playbarElement[0].offsetLeft);
+    window.addEventListener("mousemove", setFromEvent);
+    return () => {
+      window.removeEventListener("mousemove", setFromEvent);
+    };
   }, []);
 
+  const addTimeUpdate = () => {
+    const observedVideoElement = ref && ref.current;
+    if (observedVideoElement) {
+      observedVideoElement.addEventListener("timeupdate", function() {
+        setCurrentTime(observedVideoElement.currentTime);
+      })
+    }
+  }
 
-  const parse_agg_graph = () => {
+  const parse_objects = () => {
     const total_objs = new Array();
     sceneGraphs.map((sg) => {
       sg.objects.map((obj) => {
         var ind = 0;
         for (var i = 0; i < total_objs.length; i++) {
           if (total_objs[i].name == obj.name) {
-            total_objs[i].value += 1;
             ind = 1;
+            total_objs[i].value += 1;
+            
+            // last element
+            var current_dur = total_objs[i].duration[total_objs[i].duration.length - 1];
+            if (!current_dur.end || sg.frame == current_dur.end + 25 || sg.frame == 10301) {
+              current_dur.end = sg.frame;
+            }
+            else {
+              total_objs[i].duration.push({start: sg.frame, end: undefined});
+            }
+
           }
         }
+        // new object
         if (ind == 0) {
-          total_objs.push({name: obj.name, value: 1});
+          total_objs.push({name: obj.name, value: 1, duration: [{start: sg.frame, end: undefined}]});
         }
       })
-    })
-    var dataset = {
-      nodes: [],
-      edges: []
-    };
-    dataset.nodes.push({id: 1, label: "person", value: 20});
-    for (var i = 0; i < total_objs.length; i++) {
-      var node_obj = {id: i + 2, label: total_objs[i].name, value: total_objs[i].value};
-      var edge_obj = { from : 1, to: i + 2}
-      dataset.nodes.push(node_obj);
-      dataset.edges.push(edge_obj);
-    }
-    setAggGraph(dataset);
+    });
+    total_objs.sort((prev, next) => (next.value - prev.value));
+    var major_objs = total_objs.splice(0, 15);
+    setObjectList(major_objs);
   }
 
-  const parse_action_graph = () => {
-    var action_graphs = new Array();
-    atomicActions.map((action) => {
-      const objs = new Array();
-      sceneGraphs.map((sg) => {
-        if (sg.frame >= action.frame_start && sg.frame <= action.frame_end) {
-
-          sg.objects.map((obj) => {
-            var ind = 0;
-            for (var i = 0; i < objs.length; i++) {
-              if (objs[i].name == obj.name) {
-                objs[i].value += 1;
-                ind = 1;
-              }
-            }
-            if (ind == 0) {
-              objs.push({name: obj.name, value: 1});
-            }
-          })
-        }
+  const getOverlapTime = (objs) => {
+    var obj_timelist = new Array();
+    if (objs.length == 0) {
+      obj_timelist = [];
+    }
+    else if (objs.length == 1) {
+      obj_timelist = objs[0].duration.map((ele) => {
+        return {start: ele.start * 0.041, end: ele.end * 0.041}
       })
-      var dataset = {
-        nodes: [],
-        edges: []
-      };
-      dataset.nodes.push({id: 1, label: "person", value: 10});
-      for (var i = 0; i < objs.length; i++) {
-        var node_obj = {id: i + 2, label: objs[i].name, value: objs[i].value};
-        var edge_obj = { from : 1, to: i + 2}
-        dataset.nodes.push(node_obj);
-        dataset.edges.push(edge_obj);
+    } else {
+      var prevObj = objs[0];
+      var nextObj = objs[1];
+      var overlapTime = new Array();
+  
+      prevObj.duration.map((prev) => {
+        nextObj.duration.map((next) => {
+          if (prev.start >= next.start) {
+            if (prev.start >= next.end) {
+              // no overlapping
+            } 
+            else if (prev.end >= next.end) {
+              overlapTime.push({start: prev.start, end: next.end});
+            }
+            else {
+              overlapTime.push({start: prev.start, end: prev.end});
+            }
+          }
+          else if (prev.start <= next.start && prev.end >= next.start) {
+            if (prev.end >= next.end) {
+              overlapTime.push({start: next.start, end: next.end});
+            } else {
+              overlapTime.push({start: next.start, end: prev.end});
+            }
+          }
+        })
+      });
+      obj_timelist = overlapTime.map((ele) => {
+        return {start: ele.start * 0.041, end: ele.end * 0.041}
+      })
+      
+    }
+    setObjTime(obj_timelist);
+  }
+
+  const handle_object_click  = (obj) => {
+    var newObjects = new Array();
+    if (selectedObj.includes(obj)) {
+      newObjects = selectedObj.filter(item => item.name !== obj.name);
+      setObject(newObjects);
+      getOverlapTime(newObjects);
+    } else {
+      newObjects = [...selectedObj];
+      if (newObjects.length >= 2) {
+        alert ('maximum 2 objects are available');
+      } else {
+        newObjects.push(obj);
+        setObject(newObjects);
+        getOverlapTime(newObjects);
       }
-      action_graphs.push(dataset);
-    })
-    setActionGraphs(action_graphs);
+    }
   }
 
-  const graph = {
-    nodes: [
-      { id: 1, label: "Node 1", title: "node 1 tootip text" },
-      { id: 2, label: "Node 2", title: "node 2 tootip text" },
-    ],
-    edges: [
-      { from: 1, to: 2 },
-    ]
-  };
+  const handle_timeline_click = () => {
+    var newTime = playbarPosition * totalTime / 500;
+    const observedVideoElement = ref && ref.current;
+    observedVideoElement.currentTime = newTime;
+  }
 
-  const agg_options = {
-    nodes: {
-      shape: "dot",
-      scaling: {
-        label: {
-          min: 4,
-          max: 15,
-        },
-      },
-    },
-    edges: {
-
-    },
-    interaction: {
-      zoomView: false,
-    },
-    height: "300px",
-  };
-
-  const events = {
-    select: function(event) {
-      var { nodes, edges } = event;
+  const posToTime = (time) => {
+    var tmp = time * totalTime/ 500;
+    var min = (Math.floor(tmp /  60)).toString();
+    var sec = (Math.floor(tmp - min * 60)).toString();
+    if (sec.length == 1) {
+      sec = 0 + sec;
     }
-  };
+    return min + ':' + sec;
+  }
+
+  const getProgressLength = () => {
+    return currentTime * 500 / totalTime;
+  }
 
 
   return (
-    <div className="App">
-      <div>
-        <video className={"videoWrapper"} controls width="500px" src={video}></video>
-        <div className={"aggGraph"}>
-          <h1>Graph</h1>
-          {aggGraph && 
-            <Graph
-              graph={aggGraph}
-              options={agg_options}
-              events={events}
-              getNetwork={network=> {}}
-            />}
-        </div>
-      </div>
-      <h2>actions</h2>
-      <div className="atomicActions">
-        {atomicActions && atomicActions.map((ele, ind) => (
-          <div className="actionWrapper" key={ind}>
-            <div>
-              <p>{ele.class}</p>
-              {actionGraphs && 
-                <Graph
-                  graph={actionGraphs[ind]}
-                  options={agg_options}
-                  events={events}
-                  getNetwork={network=> {}}
-                />}
-            </div>
+    <div className={"navigationPage"}>
+      <h1>Navigation</h1>
+        <div className={"videoContainer"}>
+          <video className={"videoWrapper"} 
+            controls 
+            ref={ref}
+            // onLoadedMetadata = {e => {
+            //   setMetadata({
+            //     duration: e.target.duration,
+            //     current: e.target.currentTime,
+            //   })
+            // }}
+            src={video}>
+            
+          </video>
+          <div className={"playbar"} onClick={handle_timeline_click} data-tip data-for="playbarTip">
+            {objTime && objTime.map((ele, ind) => (
+              <div 
+                key={ind} 
+                className={"timeline"} 
+                style={{width: `calc(${ele.end}px - ${ele.start}px)`, left: `${ele.start}px`}} 
+              >
+              </div>
+            ))}
           </div>
-        ))}
+          {totalTime != 0 &&
+            <div className={"progressbar"} style={{width: `${getProgressLength()}px`}} data-tip data-for="playbarTip"></div>
+          }
+          <ReactTooltip id="playbarTip" place="top">
+            {posToTime(playbarPosition)}
+          </ReactTooltip>
+          <div className={"objectWrapper"}>
+            <h2>Objects</h2>
+            {objectList && objectList.map((obj, ind) => (
+              <div className={"tagWrapper"} key={ind}>
+                {selectedObj.includes(obj) 
+                  ? (<div className={"selectedTag"} onClick={() => handle_object_click(obj)}>{obj.name}</div>)
+                  : (<div className={"objectTag"} onClick={() => handle_object_click(obj)}>{obj.name}</div>)
+                }
+              </div>
+            ))}
+          </div>
+        </div>
+      <div className="infoContainer">
+        <h1>{currentTime && currentTime}</h1>
+        <h2>{totalTime}</h2>
       </div>
     </div>
   );
