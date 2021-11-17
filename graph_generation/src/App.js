d6d9f76a180491c8613ac9811d0c75e09595861e
sangkyung <skkwak9806@kaist.ac.kr>
2021-11-17 13:38:26 +0900

    graph generation added

---
+++ graph_generation/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import GraphGeneration from './GraphGeneration';
+
+function App() {
+  return (
+    <div className="App">
+      <GraphGeneration />
+    </div>
+  );
+}
+
+export default App;
---
+++ graph_generation/src/data/finding-it-annos-v0.1.1/YCII/RR/README.md
@@ -0,0 +1,1 @@
+This directory contains the `.vtt` files for the reference resolution
